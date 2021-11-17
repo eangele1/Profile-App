dc1413f09d14ec41977c3c4a14964071af80d72f
Ezer Isai Angeles <ezerangeles1@gmail.com>
2021-11-17 15:29:04 -0800

    added actual bios

--- GeneralStyles.js
+++ GeneralStyles.js
@@ -33,6 +33,7 @@ const styles = StyleSheet.create({
     fontFamily: "monospace",
     color: "white",
     marginBottom: 10,
+    padding: 10,
   },
   returnButton: {
     alignItems: "center",
--- utils/data.js
+++ utils/data.js
@@ -2,22 +2,22 @@ const DATA = [
   {
     name: "Anthony",
     image: require("../images/Anthony.jpg"),
-    bio: "This is something about me.",
+    bio: "I'm Anthony I live in Lathrop, CA. I have two degrees and a certificate in graphics design and web development. In my free time I like to play video games most of the time.",
   },
   {
     name: "Beakal",
     image: require("../images/Beakal.jpg"),
-    bio: "This is something about me.",
+    bio: "Hi team, I live in Atlanta, GA. I am a very good cook, come to my house and will serve you guys Doro wot and Kitfo. I have two beautiful daughters.",
   },
   {
     name: "Yiley",
     image: require("../images/Yiley.jpg"),
-    bio: "This is something about me.",
+    bio: "My name is Yiley Belete residing in Dallas, TX, originally from Ethiopia. I have been to a full stack development bootcamp, currently a React Native Apprentice. I like to play ping pong and with my kids.",
   },
   {
     name: "Ezer",
     image: require("../images/Ezer.jpg"),
-    bio: "This is something about me.",
+    bio: "My name is Ezer Angeles. I currently reside in Mountain View, CA, close to the tech giants of the world. I came from a 4-year university with a Bachelor of Science in Computer Science, longing to acquire experience with programming. Currently a React Native Apprentice, and I like to play video games, play and make music, and eat food in my free time!",
   },
 ];
 
